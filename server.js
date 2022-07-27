b8f8bcfd10aeb6259be646445f8bd05ece7090fd
Дима Бурлаков <tawerxo@mail.ru>
2022-07-27 08:57:08 +0300

    fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: process.env.REACT_APP_API_URL,
     credentials: true,
   },
 });
