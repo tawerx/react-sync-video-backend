0b50ea9e843541a0642fb28023e2735ca43884eb
Дима Бурлаков <tawerxo@mail.ru>
2022-07-27 08:31:02 +0300

    init

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    credentials: true,
+  },
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+  socket.on('currentTime', (time) => {
+    io.emit('currentTime', time);
+  });
+});
+
+server.listen(process.env.PORT || 3001, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log('OK serv');
+});
